7ed5b9a498f00d2b29da727e3cb8fd7753bbc1d9
GilaneZongo <launge862@gmail.com>
2026-06-17 20:27:44 -1100

    Fix QR URL params

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ export default function App() {
   const [commandes, setCommandes] = useState([]);
   const [restoId, setRestoId] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [nbTables,setNbTables]= useState(5)
  const [urlParams] = useState(() => new URLSearchParams(window.location.search));
 const qrCode = urlParams.get('code');
 const qrTable = urlParams.get('table');
