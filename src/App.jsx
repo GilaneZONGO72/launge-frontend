a0b4322e00c051476306513834bf485aced05a9a
GilaneZongo <launge862@gmail.com>
2026-06-19 16:11:00 -1100

    Ajout liste restaurants pour clients

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import QRCode from "qrcode.react";
+import { QRCodeSVG as QRCode } from "qrcode.react";
 
 const API = "https://launge-backend-production.up.railway.app";
 
